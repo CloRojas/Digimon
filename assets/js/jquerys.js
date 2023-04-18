564abc019cadf0375c88f3968e2f9c70e817dfc4
Cloooo <claudia.rojasgalaz@gmail.com>
2023-04-18 18:55:52 -0400

    new commit

--- assets/js/jquerys.js
+++ assets/js/jquerys.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
 
-$('#contenido').show(function (){
+$('#btndg').click(function (){
+  
+    $('#contenido').empty();
+    $('#rookie').empty();
         
     const url = 'https://digimon-api.vercel.app/api/digimon';
     fetch (url) 
@@ -14,16 +17,24 @@ $('#contenido').show(function (){
                const user = json[index]
                contenido = contenido + `<div class="card col-5 col-md-3 col-lg-2 m-1" > 
                                         <div class="card-body "><p class="card-title">${user.name}</p><p class="card-text"></p> 
-                                        </div><img src="${user.img}" alt="Imagen Digimon" class="card-img-bottom" width="100" height="100"><p>Nivel: ${user.level}</p></div>`
+                                        </div><img src="${user.img}" alt="Imagen Digimon" class="card-img-bottom" width="100" height="100"><p>Nivel: ${user.level}</p>
+                                       </div>`
            }
-          list.append(contenido)
-           
+          
+
+          
+           list.append(contenido); 
           
     })
+
+  
+  
 })
 
 
   $('.dropdown-item').click(function (){
+  
+    $('#contenido').empty();
      level = $(this).val();
      const url = `https://digimon-api.vercel.app/api/digimon/level/${level}`;
 
@@ -36,19 +47,42 @@ $('#contenido').show(function (){
             for (let index =0; index < json.length; index++) {
                const digimon = json[index] 
                     
-               contenido = contenido + `<div class="card col-5 col-md-3 col-lg-2 m-1" > 
+               contenido = contenido + `   
+               
+                                        <div class="card col-5 col-md-3 col-lg-2 m-1" > 
                                          <div class="card-body "><p class="card-title">${digimon.name}</p><p class="card-text"></p> 
-                                         </div><img src="${digimon.img}" alt="Imagen Digimon" class="card-img-bottom" width="100" height="100"><p>Nivel: ${digimon.level}</p></div>`
+                                         </div><img src="${digimon.img}" alt="Imagen Digimon" class="card-img-bottom" width="100" height="100"><p>Nivel: ${digimon.level}</p>
+                                         </div>`
            
                  }
-           list.append(contenido)
+                 
+                 list.empty(); 
+                 list.append(contenido); 
+
+            
+             
         
           
      })
+     $('#close').click(function() {
+        $('#rookie').empty();
+        
+        
+        $(this).hide();
+      });
+  
+   
  })
+
+ 
 
  $('#srchnmbtn').click(function() {  
     let name = $('#srchtxt').val();
+    if (!name) {
+        alert('Debes ingresar un nombre');
+        return;
+      }
+
 
      const url = `https://digimon-api.vercel.app/api/digimon/name/${name}`;
       
@@ -56,12 +90,16 @@ $('#contenido').show(function (){
         .then(response => response.json())
          .then((data) => {
 
+
             const digimon = data[0]; 
-             
-             if (!digimon) {
-                 alert('No se encontró ningún Digimon con ese nombre.');
+
+                    
+             if (!digimon ) {
+                 alert('Digimon no encontrado, por favor verifica los datos ingresados');
                  return;
                 }
+
+              
                 
             const html = `<div class="modal " id="myModal"> 
                             <div class="modal-dialog modal-sm modal-dialog-centered">
@@ -72,24 +110,35 @@ $('#contenido').show(function (){
                             <div class="modal-footer">
                              <button type="button" class="btn btn-danger close" data-bs-dismiss="modal">Close</button></div></div></div>
                              </div>`
-              
-                  const $modal = $(html);
+
+
+                             const $modal = $(html);
                  const $closeBtn = $modal.find('.close');
                 
                  $closeBtn.click(function() {
               $modal.hide();
+              $modal.empty();
           });
                 
                   $('body').append($modal);
               $modal.show();
               });
-          
-      
+                           
+
+                           
+                       
+                           
+                       
+                       
+                           
+              
+            
 
       
-     
+              
        
-            })        
+                  
           
           
+})
 })
